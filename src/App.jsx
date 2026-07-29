0516ba7eaf1e453604a900715efee4d4e5c8d823
Danny Mettler <danieljamespm@gmail.com>
2026-07-29 02:18:45 -0400

    got the input form up and working on making first fetch request to check if everything is communcating before render.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,44 @@ import './App.css'
 
 function App() {
 
+  const [searchText, setSearchText] = useState("")
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    console.log(searchText)
+
+    const apiKey = import.meta.env.VITE_API_KEY
+    const url = `https://edb-with-videos-and-images-by-ascendapi.p.rapidapi.com/api/v1/exercises/search?search=${encodeURIComponent(searchText)}`
+    const options = {
+      method: 'GET',
+      headers: {
+        'x-rapidapi-key': apiKey,
+        'x-rapidapi-host': 'edb-with-videos-and-images-by-ascendapi.p.rapidapi.com',
+        'Content-Type': 'application/json'
+      }
+    }
+  }
+
+  return (
+    <main>
+      <h1>Lift Lab</h1>
+
+      <section>
+        <form onSubmit={handleSubmit}>
+
+          <input type="text"
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+            placeholder='Search for an exercise'
+          />
+
+          <button type='Submit'>Search</button>
+
+        </form>
+      </section>
+    </main>
+  )
+
 }
 
 export default App
