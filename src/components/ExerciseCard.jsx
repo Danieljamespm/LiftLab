49d802c159ba6bc37330921d440b940c2e3466f2
Danny Mettler <danieljamespm@gmail.com>
2026-08-09 00:50:14 -0400

    got most of the exercise card put together, and created a variable to hold the exercise demo videos so i can have a conditional that chooses whether to render a video or not.

--- src/components/ExerciseCard.jsx
+++ src/components/ExerciseCard.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
 
 const ExerciseCard = ({ exercise }) => {
+
+    const video = exercise.videos?.[0]?.url
+
     return (
         <div>
             <div key={exercise.id}>
-                <h3>{exercise.name}</h3>
-                <video
-                    src={exercise.videos?.[0]?.url}
+                {video && <video
+                    src={video}
                     controls
                     width="320"
-                ></video>
+                >
+                </video>}
+                <h3>{exercise.name}</h3>
+                <span>{exercise.equipment}</span>
+                <span>{exercise.level}</span>
+                <button>View Details</button>
+                <button>+ Add To Routine</button>
+
             </div>
         </div>
     )
