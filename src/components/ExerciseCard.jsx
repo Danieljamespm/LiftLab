416528260fc42374f4613974354f29cefd0a2d28
Danny Mettler <danieljamespm@gmail.com>
2026-08-09 01:08:11 -0400

    added class names to begin styling exercise card.

--- src/components/ExerciseCard.jsx
+++ src/components/ExerciseCard.jsx
@@ -5,35 +5,35 @@ const ExerciseCard = ({ exercise }) => {
     const video = exercise.videos?.[0]?.url
 
     return (
-        <div>
-            <div key={exercise.id}>
 
-                <div>
-                    {video ? (
-                        <video
-                            src={video}
-                            controls
-                            width="320"
-                        />
-                    ) : (
-                        <div>No Preview Available</div>
-                    )}
-                </div>
+        <div className='exercise-card'>
 
-                <h3>{exercise.name}</h3>
-
-                <div>
-                    <span>{exercise.equipment}</span>
-                    <span>{exercise.level}</span>
-                </div>
-
-                <div>
-                    <button>View Details</button>
-                    <button>+ Add To Routine</button>
-                </div>
-
+            <div className='media-container'>
+                {video ? (
+                    <video
+                        src={video}
+                        controls
+                        width="320"
+                    />
+                ) : (
+                    <div>No Preview Available</div>
+                )}
             </div>
+
+            <h3>{exercise.name}</h3>
+
+            <div className='badge-container'>
+                <span>{exercise.equipment}</span>
+                <span>{exercise.level}</span>
+            </div>
+
+            <div className='btn-container'>
+                <button>View Details</button>
+                <button>+ Add To Routine</button>
+            </div>
+
         </div>
+
     )
 }
 
