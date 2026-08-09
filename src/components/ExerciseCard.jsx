d812d7fff729243b94bbabd3b41a415228bd465a
Danny Mettler <danieljamespm@gmail.com>
2026-08-09 00:36:15 -0400

    linked the exercise component and build routine exercise search together using props.

--- src/components/ExerciseCard.jsx
+++ src/components/ExerciseCard.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const ExerciseCard = ({ exercise }) => {
     return (
-        <div>ExerciseCard</div>
+        <div>
+            <div key={exercise.id}>
+                <h3>{exercise.name}</h3>
+                <video
+                    src={exercise.videos?.[0]?.url}
+                    controls
+                    width="320"
+                ></video>
+            </div>
+        </div>
     )
 }
 
