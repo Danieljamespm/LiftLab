6cde015b579de3ee29455e8b721de07fdb770b8d
Danny Mettler <danieljamespm@gmail.com>
2026-08-11 02:22:52 -0400

    got the fetch request working in a useEffect and working on getting the data I want from the response to render to details page.

--- src/pages/ExerciseDetails.jsx
+++ src/pages/ExerciseDetails.jsx
@@ -10,10 +10,63 @@ const ExererciseDetails = () => {
 
     const { id } = useParams()
 
+    const imageBaseUrl = "https://raw.githubusercontent.com/yuhonas/free-exercise-db/main/exercises/"
+
+    useEffect(() => {
+
+
+
+        const fetchExercise = async () => {
+            setLoading(true)
+            setError("")
+
+            try {
+                const response = await fetch(`https://api.exerciseapi.dev/v1/exercises/${id}`,
+                    {
+                        method: "GET",
+                        headers: {
+                            "X-API-Key": import.meta.env.VITE_API_KEY
+                        }
+                    }
+                )
+                if (!response.ok) {
+                    throw new Error("Failed to load exercise details..")
+                }
+
+
+                const data = await response.json()
+
+                console.log(data)
+
+                setExercise(data.data)
+
+            } catch (error) {
+                setError(error.message)
+            } finally {
+                setLoading(false)
+            }
+
+
+
+        }
+
+        fetchExercise()
+
+    }, [id])
+
     return (
         <div>
             <h1>Exercise Details</h1>
-            <p>{id}</p>
+            {loading && <p>Loading Details..</p>}
+            {error && <p>{error}</p>}
+
+            <div>
+                <h2>{exercise?.name}</h2>
+                <img src={imageBaseUrl + exercise?.images[1]} alt="Exercise Demo Image" />
+                <p>Equipment: {exercise?.equipment}</p>
+                <p>Primary Muscles: {exercise?.primaryMuscles[0]}</p> <p>{exercise?.primaryMuscles[1]}</p>
+                <p>Overview: {exercise?.overview}</p>
+            </div>
 
         </div>
     )
