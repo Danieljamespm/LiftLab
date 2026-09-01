5978f45c13fae7403c5e74f08aa80109686133cb
Danny Mettler <danieljamespm@gmail.com>
2026-09-01 01:53:17 -0400

    got server set up with its first route running to routineRoutes.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config()
 
 const express = require("express")
+const routineRoutes = require("./routes/routineRoutes")
 
 const app = express()
 
@@ -8,30 +9,11 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.json())
 
-app.get("/", (req, res) => {
-    res.send("LiftLab Backend is alive")
-})
+app.use("/api/routines", routineRoutes)
 
-app.get("/api/test", (req, res) => {
-    res.json({
-        message: "LiftLab API is working with nodemon"
-    })
-})
 
-app.get("/api/routines", (req, res) => {
-    res.json({
-        message: "Routine route is working"
-    })
-})
 
-app.post("/api/routines", (req, res) => {
-    console.log(req.body)
 
-    res.json({
-        message: "Routine recieved",
-        routine: req.body,
-    })
-})
 
 app.listen(5000, () => {
     console.log(`Server is running on port ${PORT}`)
