fa85b4f4cd508ad05a6f27524b0478a9649cbaf3
Danny Mettler <danieljamespm@gmail.com>
2026-09-11 00:59:43 -0400

    added some error handling for all the controllers for invalid ID routes and missing params for post requests.

--- server/controllers/routineController.js
+++ server/controllers/routineController.js
@@ -7,33 +7,73 @@ const getRoutines = async (req, res) => {
 }
 
 const createRoutine = async (req, res) => {
-    const { routineName, routineExercises } = req.body
 
-    const routine = await Routine.create({
-        routineName,
-        routineExercises
-    })
+    try {
+        const { routineName, routineExercises } = req.body
 
-    res.status(201).json(routine)
+        const routine = await Routine.create({
+            routineName,
+            routineExercises
+        })
+
+        res.status(201).json(routine)
+    } catch (error) {
+        return res.status(400).json({ message: "Invalid routine data" })
+    }
+
 }
 
 const getRoutineById = async (req, res) => {
-    const routine = await Routine.findById(req.params.id)
 
-    res.status(200).json(routine)
+    try {
+        const routine = await Routine.findById(req.params.id)
+
+        if (!routine) {
+            return res.status(404).json({ message: "Routine not found" })
+        }
+
+        res.status(200).json(routine)
+
+    } catch (error) {
+        return res.status(400).json({ messge: "Invalid routine ID" })
+    }
+
+
+
 }
 
 
 const deleteRoutine = async (req, res) => {
-    const routine = await Routine.findByIdAndDelete(req.params.id)
 
-    res.status(200).json(routine)
+    try {
+        const routine = await Routine.findByIdAndDelete(req.params.id)
+
+        if (!routine) {
+            return res.status(404).json({ message: "Routine not found" })
+        }
+
+        res.status(200).json(routine)
+
+    } catch (error) {
+        return res.status(400).json({ message: "Invalid routine ID" })
+    }
+
 }
 
 const updateRoutine = async (req, res) => {
-    const routine = await Routine.findByIdAndUpdate(req.params.id, req.body, { new: true })
 
-    res.status(200).json(routine)
+    try {
+        const routine = await Routine.findByIdAndUpdate(req.params.id, req.body, { new: true })
+
+        if (!routine) {
+            return res.status(404).json({ message: "Routine not found" })
+        }
+
+        res.status(200).json(routine)
+    } catch (error) {
+        return res.status(400).json({ message: "Invalid routine ID" })
+    }
+
 }
 
 
