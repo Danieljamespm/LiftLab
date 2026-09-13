e5d68a07c334204a4826c535cc280f5e11a562d7
Danny Mettler <danieljamespm@gmail.com>
2026-09-12 21:41:57 -0400

    added bcrypt for password hashing.

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,12 +1,15 @@
 const User = require("../models/User")
+const bcrypt = require("bcrypt")
 
 const registerUser = async (req, res) => {
     const { name, email, password } = req.body
 
+    const hashedPassword = await bcrypt.hash(password, 10)
+
     const user = await User.create({
         name,
         email,
-        password
+        password: hashedPassword
     })
 
     res.status(201).json(user)
