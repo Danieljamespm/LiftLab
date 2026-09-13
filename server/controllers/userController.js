062fc95bfc9663a13b565fff562c8478573f6401
Danny Mettler <danieljamespm@gmail.com>
2026-09-12 22:22:07 -0400

    added a try catch to handle any other errors such as missing data or unfamiliar data.

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,27 +2,33 @@ const User = require("../models/User")
 const bcrypt = require("bcrypt")
 
 const registerUser = async (req, res) => {
-    const { name, email, password } = req.body
 
-    const userExists = await User.findOne({ email })
+    try {
+        const { name, email, password } = req.body
 
-    if (userExists) {
-        return res.status(400).json({ message: "User already exists" })
+        const userExists = await User.findOne({ email })
+
+        if (userExists) {
+            return res.status(400).json({ message: "User already exists" })
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10)
+
+        const user = await User.create({
+            name,
+            email,
+            password: hashedPassword
+        })
+
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+        })
+    } catch (error) {
+        return res.status(400).json({ message: "Invalid user data" })
     }
 
-    const hashedPassword = await bcrypt.hash(password, 10)
-
-    const user = await User.create({
-        name,
-        email,
-        password: hashedPassword
-    })
-
-    res.status(201).json({
-        _id: user._id,
-        name: user.name,
-        email: user.email,
-    })
 }
 
 
