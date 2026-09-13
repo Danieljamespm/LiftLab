646696ecf1e0e663861ad9ed695ba274ad2204a6
Danny Mettler <danieljamespm@gmail.com>
2026-09-12 22:41:54 -0400

    added login route and login controller to handle the beginning process of auth.

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -31,7 +31,34 @@ const registerUser = async (req, res) => {
 
 }
 
+const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body
+
+        const user = await User.findOne({ email })
+
+        if (!user) {
+            return res.status(401).json({ message: "Invalid email or password" })
+        }
+
+        const passwordMatches = await bcrypt.compare(password, user.password)
+
+        if (!passwordMatches) {
+            return res.status(401).json({ message: "Invalid email or password" })
+        }
+
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+        })
+    } catch (error) {
+
+    }
+}
+
 
 module.exports = {
     registerUser,
+    loginUser,
 }
