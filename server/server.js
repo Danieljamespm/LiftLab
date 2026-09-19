cf57d6ff230ab28af6a16417e70d3faf73181096
Danny Mettler <danieljamespm@gmail.com>
2026-09-18 21:56:55 -0400

    starting to add routes and controllers for starting a workout in liftlab.

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const express = require("express")
 const connectDB = require("./config/db")
 const routineRoutes = require("./routes/routineRoutes")
 const userRoutes = require("./routes/userRoutes")
+const workoutRoutes = require("./routes/workoutRoutes")
 
 
 
@@ -18,6 +19,7 @@ app.use(express.json())
 
 app.use("/api/routines", routineRoutes)
 app.use("/api/users", userRoutes)
+app.use("/api/workouts", workoutRoutes)
 
 
 
