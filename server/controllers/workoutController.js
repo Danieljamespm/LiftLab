ebb1adb867e591d4aae573b16fa3599251ecee48
Danny Mettler <danieljamespm@gmail.com>
2026-09-19 00:30:32 -0400

    finished create workout route and controller, now working on getWorkout route and controller.

---
+++ server/controllers/workoutController.js
@@ -0,0 +1,45 @@
+const Workout = require("../models/Workout")
+const Routine = require("../models/Routine")
+
+const createWorkout = async (req, res) => {
+
+    try {
+        const { routine, workoutName, workoutExercises } = req.body
+
+        if (!workoutName) {
+            return res.status(400).json({ message: "Workout name is required" })
+        }
+        if (routine) {
+
+            const routineExist = await Routine.findOne({ _id: routine, user: req.user._id })
+
+
+            if (!routineExist) {
+                return res.status(404).json({ message: "Routine not found" })
+            }
+        }
+
+
+        const workout = await Workout.create({
+            routine,
+            workoutName,
+            workoutExercises,
+            user: req.user._id
+        })
+        res.status(201).json(workout)
+    } catch (error) {
+
+        return res.status(500).json({ message: "Server Error" })
+    }
+
+}
+
+const getWorkout = async (req, res) => {
+
+}
+
+
+module.exports = {
+    createWorkout,
+    getWorkout,
+}
--- server/routes/workoutRoutes.js
+++ server/routes/workoutRoutes.js
@@ -1,8 +1,15 @@
 const express = require("express")
 
+
+const {
+    createWorkout,
+    getWorkout,
+} = require("../controllers/workoutController")
+const protect = require("../middleware/authMiddleware")
+
 const router = express.Router()
 
-
-router.post("/")
+router.get("/", protect, getWorkout)
+router.post("/", protect, createWorkout)
 
 module.exports = router
