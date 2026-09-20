88f7943b6c6b6a9b075ce3d3cd66c070369d4c41
Danny Mettler <danieljamespm@gmail.com>
2026-09-19 22:20:28 -0400

    added login page where users can log in, working on connecting my server and sending a successful POST request.

---
+++ src/pages/Login.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { useState } from 'react'
+
+const Login = () => {
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+
+        const response = await fetch("http://localhost:5000/api/users/login", {
+            method: "POST",
+
+            headers: {
+                "Content-Type": "application/json"
+            },
+
+            body: JSON.stringify({
+                email,
+                password
+            })
+        })
+
+        console.log(response)
+
+    }
+
+    return (
+        <>
+            <h1>Login</h1>
+            <form onSubmit={handleSubmit}>
+                <input type="text"
+                    value={email}
+                    placeholder='Email'
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <input type="password"
+                    value={password}
+                    placeholder='Password'
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+
+                <button>Login</button>
+            </form>
+
+
+
+
+        </>
+    )
+}
+
+export default Login
